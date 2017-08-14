b747db942f2e8b0cfe2263183b75ddda6795fb40
Yakeing <yakeing@gmail.com>
2017-08-14 20:50:59 +0800

    Lowercase letters

--- tests/php_curlTest/php_curlTest.php
+++ tests/php_curlTest/php_curlTest.php
@@ -1,15 +1,15 @@
 <?php
-namespace php_CurlTest;
+namespace php_curlTest;
 
-use php_Curl;
-use php_Curl\Curl;
+use php_curl;
+use php_curl\curl;
 use PHPUnit\Framework\TestCase;
 
-class php_CurlTest extends TestCase{
+class php_curlTest extends TestCase{
 
   public function testGet(){
     $url = "https://raw.githubusercontent.com/yakeing/php_Curl/master/tests/php_CurlTest/return";
-    $Curl = new Curl();
+    $Curl = new curl();
     $Curl->Timeout = 10;
     $Curl->Referer = 'https://github.com/';
     $Curl->Encoding = 'gzip';
