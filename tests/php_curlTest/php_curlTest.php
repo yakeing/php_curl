ceb30f347dcb7819b8bb3895e5e4231fa4f8feba
Yakeing <yakeing@gmail.com>
2017-08-15 09:45:31 +0800

    up UserAgent

--- tests/php_curlTest/php_curlTest.php
+++ tests/php_curlTest/php_curlTest.php
@@ -13,6 +13,7 @@ class php_curlTest extends TestCase{
     $curl->Timeout = 10;
     $curl->Referer = 'https://github.com/';
     $curl->Encoding = 'gzip';
+    $curl->UserAgent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36';
     $this->assertTrue($curl->Get($url));
     $this->assertEquals(200, $curl->HttpCode);
     $this->assertEquals('Hello world', trim($curl->Body));
