58c241bcd5594d71d1e83eba84f41a19ade7d2ed
Yakeing <yakeing@gmail.com>
2017-08-30 07:06:00 +0800

    update i18n documentation

--- src/php_curl/curl.php
+++ src/php_curl/curl.php
@@ -3,9 +3,9 @@
   * Curl Class
   *
   * @author http://weibo.com/yakeing
-  * @version 2.1
+  * @version 2.2
   * Prompt: CertUrl The server may not contain root / chain SSL certificates, causing authentication errors
-  * 注意: CertUrl 服务器可能不包含根/链SSL证书,导致身份验证错误
+  * 注意: 服务器证书可能不包含根/链SSL证书,导致身份验证错误
   * Prompt: When you open the {HeadOut} option, GET/POST returns only false
   * 注意:当您打开{HeadOut}选项时,GET / POST仅返回false
   *
@@ -19,32 +19,33 @@ class curl{
 	public $Body = '';
 	public $HttpCode = 0;
 	public $HttpError = '';
-	public $HttpUrl = ''; //有效发送地址(最后一个)
+	public $HttpUrl = ''; //最后一个有效发送地址
 	public $HeadersOut = ''; //发送的头部
 	public $HttpCookie = array(); //返回 Cookie
 
-	public $Cookie = array(); //设置Cookie
+	public $CurlOpt = array(); //自定义 Curl 选项
+	public $Cookie = array(); //设置 Cookie
 	public $Timeout = 5; //运行时间
 	public $Referer = ''; //伪装来源
-	public $CertUrl = ''; //HTTPS地址SSL证书
+	public $CertUrl = ''; //HTTPS地址SSL证书路径
 	public $HeadersOn = false; //输出头部信息
-	public $HeaderFunction = false; //回调函数(头部)
+	public $HeaderFunction = false; //回调函数(返回头部)
 	public $HeadOut = false; //回调函数(只输出头部)
-	public $LocationQuantity = 0; //自动重定向次数(301/302)
+	public $LocationQuantity = 0; //自动重定向次数(HTTP 301/302)
 	public $Encoding = 'deflate, gzip'; //压缩 1.identity、2.deflate, gzip
-	public $UserAgent = 'Mozilla/5.0 (compatible; Baiduspider/2.0; +http://www.baidu.com/search/spider.html)';
+	public $UserAgent = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html )';
 
 	//Constructor
 	public function __construct(){
 		if(function_exists('curl')){
-			throw new Exception('php需要Curl函数支持或被关闭');
+			throw new Exception('Curl not defined');
 		}
 	} //END __construct
 
 	//Get Network Resources
 	public function Get($Url, $Header = array()){
 		$Options = $this->Options($Url, $Header);
-		//$Options[CURLOPT_HTTPGET] = true; //默认
+		//$Options[CURLOPT_HTTPGET] = true; //default setting
 		return $this->Http($Options);
 	} //NDE Get
 
@@ -59,52 +60,63 @@ class curl{
 	//Curl Options
 	private function Options($Url, $Header){
 		$Options = array(
-			CURLOPT_URL => $Url, //URL 地址
-			CURLOPT_USERAGENT => $this->UserAgent, //客户UA标示
-			CURLOPT_TIMEOUT => $this->Timeout, //运行秒
-			CURLOPT_CONNECTTIMEOUT => $this->Timeout, //连接秒
-			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1, //http 1.1版本
-			CURLOPT_RETURNTRANSFER => true, //返回文件流
+			CURLOPT_URL => $Url, //Destination address(URL)
+			CURLOPT_USERAGENT => $this->UserAgent, //Customer UA mark
+			CURLOPT_TIMEOUT => $this->Timeout, //Running time (sec)
+			CURLOPT_CONNECTTIMEOUT => $this->Timeout, //Connection time (sec)
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1, //http 1.1 Edition
+			CURLOPT_RETURNTRANSFER => true, //Return file stream
 			//CURLOPT_COOKIESESSION => true, //session cookie
-			CURLINFO_HEADER_OUT => true //追踪句柄的请求
+			CURLINFO_HEADER_OUT => true //Request for trace handle
 		);
-		//不输出Body(无法接收cookie)
+		//Closing Body will not be able to receive cookie information
 		//$Options[CURLOPT_NOBODY] = true;
-		if(!empty($Header)){//设置头部信息
+		if(!empty($Header)){//Set header information
 			$Options[CURLOPT_HTTPHEADER] = $Header;
 		}
-		if(strrpos($Url, 'https://') === 0){//https模式
+		if(strrpos($Url, 'https://') === 0){//https mode
 			if(empty($this->CertUrl)){
-				$Options[CURLOPT_SSL_VERIFYPEER] = false;//https安全
-				$Options[CURLOPT_SSL_VERIFYHOST] = false;//证书检查
+				$Options[CURLOPT_SSL_VERIFYPEER] = false;//https peer’s certificate OFF
+				$Options[CURLOPT_SSL_VERIFYHOST] = false;//No inspection certificate
 			}else{
-				$Options[CURLOPT_SSL_VERIFYPEER] = true;//https安全
-				$Options[CURLOPT_SSL_VERIFYHOST] = 2;//严格认证
-				$Options[CURLOPT_CAINFO] = $this->CertUrl;//证书地址
+				$Options[CURLOPT_SSL_VERIFYPEER] = true;//Validation of peer’s certificate
+				$Options[CURLOPT_SSL_VERIFYHOST] = 2;//Does the Certificate Common Name match the hostname (Strict certification)
+				$Options[CURLOPT_CAINFO] = $this->CertUrl;//Certificate address
 			}
 		}
-		if(is_int($this->LocationQuantity) && $this->LocationQuantity > 0){//递归 自动重定向次数(301/302)
-			$Options[CURLOPT_FOLLOWLOCATION] = true;//重定向
-			$Options[CURLOPT_AUTOREFERER] = true;//重定向自动设置Referer:信息
-			$Options[CURLOPT_MAXREDIRS] = $this->LocationQuantity; //重定向次数
+		if(is_int($this->LocationQuantity) && $this->LocationQuantity > 0){//redirection (HTTP 301/302)
+			$Options[CURLOPT_FOLLOWLOCATION] = true;//Open redirection
+			$Options[CURLOPT_AUTOREFERER] = true;//Redirection automatically sets Referer information
+			$Options[CURLOPT_MAXREDIRS] = $this->LocationQuantity; //Redirection times
 		}
-		if(!empty($this->Cookie)){//Cookie信息
-			$Options[CURLOPT_COOKIE] = $this->Cookie;
+		if(!empty($this->Cookie)){//Cookie information
+			if(is_array($this->Cookie)){
+				$cookie = '';
+				foreach ($this->Cookie as $key => $value) {
+					$cookie .= $key.'='.$value.';';
+				}
+			}else{ //is_string($this->Cookie)
+				$cookie = $this->Cookie;
+			}
+			$Options[CURLOPT_COOKIE] = $cookie;
 		}
-		if(!empty($this->Referer)){//伪装来源
+		if(!empty($this->Referer)){//Camouflage source
 			$Options[CURLOPT_REFERER] = $this->Referer;
 		}
-		if(!empty($this->Encoding)){//压缩
+		if(!empty($this->Encoding)){//compress
 			$Options[CURLOPT_ENCODING] = $this->Encoding;
 		}
-		if($this->HeadersOn === true){//返回头部
+		if($this->HeadersOn === true){//Back header
 			$Options[CURLOPT_HEADER] = true;
 		}
-		if($this->HeaderFunction === true){//回调函数(获取头部)
+		if($this->HeaderFunction === true){//callback (Get header)
 			$Options[CURLOPT_HEADERFUNCTION] = array($this, 'HeaderFunction');
 		}
+		if(count($this->CurlOpt)>0){//Add or change Curl Options
+			$Options = array_merge($Options, $this->CurlOpt);
+		}
 		return $Options;
-	}
+	} //END Options
 
 	// Header Function
 	private function HeaderFunction($thising, $header){
@@ -131,15 +143,15 @@ class curl{
 		$ch = curl_init();
 		curl_setopt_array($ch, $options);
 		$ResUlt = curl_exec($ch);
-		//获取 header  部分的大小
+		//Gets the size of the header
 		$Header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-		//最后一个有效的URL地址
+		//Gets the last valid URL
 		$this->HttpUrl = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
-		//发送的头部请求
+		//Send header request
 		$this->HeadersOut = curl_getinfo($ch, CURLINFO_HEADER_OUT );
-		//获取最后一个代码
+		//Get the last valid code
 		$this->HttpCode = (int)curl_getinfo($ch, CURLINFO_HTTP_CODE);
-		//获取Content-Type:值
+		//Get Content-Type value
 		//$ContentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE );
 		if($ResUlt === false || curl_errno($ch) !== 0){
 			$this->error = curl_error($ch);
